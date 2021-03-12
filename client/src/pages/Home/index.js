78ff3b812354485a272cf0dd51bf9019d6b14c85
littlefire <aybwhite@gmail.com>
2021-03-12 09:20:34 -0800

    hooking up the main page to be connected to the logout function - will be transfered to nav - edited index.js and back up css

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,9 +1,24 @@
-function Home() {
-    return (
-        <div>
-            <p>Home Page</p>
-        </div>
-    )
+import React from "react";
+
+//We could turn this into a Navbar or link it?
+//Handles logout function
+function Home({ handleLogOut }) {
+  return (
+    <div className="home">
+      <div className="login-divider">
+        <button className="logout-button" onClick={handleLogOut}>
+          Logout
+        </button>
+
+        <h1>Coin Creatif</h1>
+        
+      </div>
+      <div>
+      <p>Welcome...</p>
+
+      </div>
+    </div>
+  );
 }
 
 export default Home;
--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -1,9 +1,87 @@
-function Login() {
-    return (
-        <div>
-            <p>Login Page</p>
+import React from "react";
+//import "./login.css";
+
+const Login = (props) => {
+  const {
+    email,
+    setEmail,
+    password,
+    setPassword,
+    handleLogin,
+    handleSignup,
+    hasAccount,
+    setHasAccount,
+    emailError,
+    passwordError
+  } = props;
+
+
+  return (
+    
+    //React Fragment
+    //onClick in span = toggle
+    <section className="login">
+      <div className="loginContainer">
+      <div className="btnContainer">
+          {hasAccount ? (
+            <>
+              <h1 className="loginTitle" onClick={handleLogin}>Sign In</h1>
+            </>
+          ) : (
+            <>
+              <h1 className="loginTitle" onClick={handleLogin}>Sign up</h1>
+            </>
+          )}
         </div>
-    )
-}
+
+        <label>Email</label>
+        <input
+          type="text"
+          required
+          autoFocus
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <p className="errorMsg">{emailError}</p>
+        <label>Password</label>
+        <input
+          type="password"
+          required
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <p className="errorMsg">{passwordError}</p>
+        <div className="btnContainer">
+          {hasAccount ? (
+            <>
+              <button onClick={handleLogin}>Sign In</button>
+
+              <p>
+                Don't have a account?
+                <span onClick={() => setHasAccount(!hasAccount)}>Sign Up</span>
+              </p>
+              <p>
+                HERE WILL BE GOOGLE/FB LOGIN
+              </p>
+            </>
+          ) : (
+            <>
+              <button onClick={handleSignup}>Sign up</button>
+              
+              <p>
+                Have and account ?
+                <span onClick={() => setHasAccount(!hasAccount)}>Sign in</span>
+              </p>
+
+              <p>
+                HERE WILL BE GOOGLE/FB LOGIN
+              </p>
+            </>
+          )}
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Login;
