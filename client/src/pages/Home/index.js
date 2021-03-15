c631396eb2bca6fc2cf2b50695fc4ec952489509
littlefire <aybwhite@gmail.com>
2021-03-15 14:11:27 -0700

    added home sign out button

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,7 +1,12 @@
+import React from "react";
+import app from "./base";
+
 function Home() {
+
     return (
         <div>
-            <p>Home Page</p>
+           <h1>Home</h1>
+            <button onClick={() => app.auth().signOut()}>Sign out</button>
         </div>
     )
 }
