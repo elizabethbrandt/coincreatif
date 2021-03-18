abbdd6c1cab15294685eed1ba6dc99fae03de7e4
Elizabeth Brandt <ebrandt8@uw.edu>
2021-03-17 20:37:45 -0700

    playing around with styles on home page

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,7 +1,24 @@
+import { Button, Typography } from "@material-ui/core";
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+    primaryStyle: {
+        fontStyle: "oblique"
+    },
+    buttonStyles: {
+        color: "green"
+    }
+});
+
 function Home() {
+    const classes = useStyles();
+
     return (
         <div>
-            <p>Home Page</p>
+            <Typography className={classes.primaryStyle} color="primary" variant="h1">Coin Créatif</Typography>
+            <Typography className={classes.primaryStyle} color="primary" variant="h3">Where small businesses thrive</Typography>
+            <Button color="primary" variant="outlined">Browse the Market</Button>
+            
         </div>
     )
 }
