d7ac88f41568e632491b82cc227feede67ba6d35
Jailanie Kaganovsky <jnctw33ty@gmail.com>
2021-03-18 23:06:03 -0700

    Placed mongoose connection back and updated path to routes folder.

--- server.js
+++ server.js
@@ -1,30 +1,26 @@
 const express = require("express");
-
-const routes = require("./routes/uploadRoutes");
 const app = express();
 const cors = require("cors");
-const dbConfig = require("./config/db");
 const PORT = process.env.PORT || 3001;
+const imageRouter = require("./routes/imageRoute");
+const mongoose = require("mongoose");
 
-const api = require("./routes/uploadRoutes");
-const multerUploads = require("./middleware/multer");
-const { ImageUploadRouter } = require("./routes/uploadRoutes");
-
-// Connect to MongDB
-require("./config/db");
-
-// Define middleware here
+// Adding the middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
 // Add routes, both API and view
-app.use("/api", ImageUploadRouter);
+app.use("/", imageRouter);
 app.use(cors());
 app.use("/public", express.static("public"));
-app.use("/api", api);
+
+// Connection to MongDB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/coincreatif");
 
 // Start the API server
 app.listen(PORT, () => {
