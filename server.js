a882fca59e4de20c612f87aa6d53b33bf63a93a6
francine babauta <francine.babauta@outlook.com>
2021-03-20 10:30:31 -0700

    added index.js for checkout

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const express = require("express");
-
+const uuid = require("uuid");
 const routes = require("./routes/uploadRoutes");
 const app = express();
 const cors = require("cors");
 const dbConfig = require("./config/db");
-
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const api = require("./routes/uploadRoutes");
 const multerUploads = require("./middleware/multer");
 const { ImageUploadRouter } = require("./routes/uploadRoutes");
 
-require("dotenv").config();
+
+// Start the API server
+const port = process.env.PORT || 3000;
 
 
 // Connect to MongDB
@@ -37,23 +39,62 @@ app.use((req, res, next) => {
   next();
 });
 
+// Define Stripe here
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/", require("./routes/products"));
-app.use("/payment", require("./routes/payment"));
-
-app.get("/checkout", (req, res) => {
-  res.render("checkout");
+app.get("/", (req, res) => {
+  res.send("Add your Stripe Secret Key to the .require('stripe') statement!");
 });
 
-app.all("*", (req, res) => {
-  res.render("pages.404");
+app.post("/checkout", async (req, res) => {
+  console.log("Request:", req.body);
+
+  let error;
+  let status;
+  try {
+    const { product, token } = req.body;
+
+    const customer = await stripe.customers.create({
+      email: token.email,
+      source: token.id
+    });
+
+    const idempotency_key = uuid();
+    const charge = await stripe.charges.create(
+      {
+        amount: product.price * 100,
+        currency: "usd",
+        customer: customer.id,
+        receipt_email: token.email,
+        description: `Purchased the ${product.name}`,
+        shipping: {
+          name: token.card.name,
+          address: {
+            line1: token.card.address_line1,
+            line2: token.card.address_line2,
+            city: token.card.address_city,
+            country: token.card.address_country,
+            postal_code: token.card.address_zip
+          }
+        }
+      },
+      {
+        idempotency_key
+      }
+    );
+    console.log("Charge:", { charge });
+    status = "success";
+  } catch (error) {
+    console.error("Error:", error);
+    status = "failure";
+  }
+
+  res.json({ error, status });
 });
 
 
-// Start the API server
-const port = process.env.PORT || 3000;
+
 app.listen(port, () => {
   console.log("server working at http://localhost:" + port);
 });
