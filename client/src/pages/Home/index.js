e3aca812338e3817834f6e16b14b7f319d7de916
littlefire <aybwhite@gmail.com>
2021-03-20 13:36:45 -0700

    added carousel into main page

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,26 +1,97 @@
-import { Button, Typography } from "@material-ui/core";
-import { makeStyles } from '@material-ui/core/styles';
+import React from 'react';
+import Carousel from 'react-material-ui-carousel';
+import autoBind from 'auto-bind';
+import {
+    Paper
+} from '@material-ui/core'
 
-
-const useStyles = makeStyles({
-    primaryStyle: {
-        fontStyle: "oblique"
-    },
-    buttonStyles: {
-        color: "green"
-    }
-});
-
-function Home() {
-    const classes = useStyles();
-
+function Project(props) {
     return (
-        <div>
-            <Typography className={classes.primaryStyle} color="primary" variant="h1">Coin Créatif</Typography>
-            <Typography className={classes.primaryStyle} color="primary" variant="h3">Where small businesses thrive</Typography>
-            <Button color="primary" variant="outlined">Browse the Market</Button>
-        </div>
+        <Paper
+            className="Project"
+            style={{
+                backgroundColor: props.item.color,
+            }}
+            elevation={10}
+        >
+            <h2>{props.item.name}</h2>
+
+            <img alt="product" src={props.item.image} style={{height: props.item.height, maxHeight: props.item.maxHeight, width: props.item.width, objectFit: props.item.objectFit}}/>
+
+            <p>{props.item.description}</p> 
+        </Paper>
     )
 }
 
-export default Home;
+const items = [
+    {
+        image: "https://www.nasa.gov/sites/default/files/thumbnails/image/blackhole.png",
+        height: "auto",
+        maxHeight: "300px",
+        width: "100%",
+        objectFit: "cover"
+    },
+    {
+        image: "https://www.nasa.gov/sites/default/files/styles/full_width/public/thumbnails/image/2008_m87_labeled.jpg?itok=0OP-A6Eq",
+        height: "auto",
+        maxHeight: "300px",
+        width: "100%",
+        objectFit: "cover"
+    },
+    {
+        image: "https://www.nasa.gov/sites/default/files/thumbnails/image/blackhole.png",
+        height: "auto",
+        maxHeight: "300px",
+        width: "100%",
+        objectFit: "cover"
+    },
+    {
+        image: "https://www.nasa.gov/sites/default/files/styles/full_width/public/thumbnails/image/2008_m87_labeled.jpg?itok=0OP-A6Eq",
+        height: "auto",
+        maxHeight: "300px",
+        width: "100%",
+        objectFit: "cover"
+    }
+]
+
+export default class MyProjectsExample extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            autoPlay: true,
+            animation: "fade",
+            indicators: true,
+            timeout: 800,
+            navButtonsAlwaysInvisible: true
+        }
+
+        autoBind(this);
+    }
+
+    render() {
+        return (
+            <div style={{ color: "#494949" }}>
+                <h2>Coin Créatif</h2>
+
+                <Carousel
+                    style={{ color: "#494949" }}
+                    className="SecondExample"
+                    autoPlay={this.state.autoPlay}
+                    animation={this.state.animation}
+                    indicators={this.state.indicators}
+                    timeout={this.state.timeout}
+                    navButtonsAlwaysInvisible={this.state.navButtonsAlwaysInvisible}
+                
+                >
+                    {
+                        items.map((item, index) => {
+                            return <Project item={item} key={index} />
+                        })
+                    }
+                </Carousel>
+
+            </div>
+        )
+    }
+}
