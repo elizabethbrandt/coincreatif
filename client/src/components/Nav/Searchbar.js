6dc49edf2085fbf7d4b1741ecee0cd9ca7f12213
littlefire <aybwhite@gmail.com>
2021-03-23 15:12:58 -0700

    semi working search bar- only working on its own route

---
+++ client/src/components/Nav/Searchbar.js
@@ -0,0 +1,54 @@
+import ProductInfo from '../../seeds';
+
+import React, { Component } from 'react';
+
+class Search extends Component {
+
+  constructor(){
+    super();
+
+    this.state={
+      search:null
+    };
+  }
+
+  searchSpace=(event)=>{
+    let keyword = event.target.value;
+    this.setState({search:keyword})
+  }
+
+  render(){
+    
+    // eslint-disable-next-line array-callback-return
+    const items = ProductInfo.filter((data)=>{
+      if(this.state.search == null)
+          return data
+      else if(
+         data.itemName.toLowerCase().includes(this.state.search.toLowerCase()) 
+      || data.category.toLowerCase().includes(this.state.search.toLowerCase())){
+          return data
+      }
+    }).map(data=>{
+      return(
+      <div>
+        <ul>
+          <li style={{position:'relative',left:'10vh'}}>
+            <span >{data.itemName}</span>
+            <span >{data.category}</span>
+            <span >{data.description}</span>
+          </li>
+        </ul>
+      </div>
+      )
+    })
+
+    return (
+      <div>
+      <input type="text" placeholder="Enter item to be searched" onClick={(e)=>this.searchSpace(e)} />
+      {items}
+      </div>
+    )
+  }
+}
+
+export default Search;
