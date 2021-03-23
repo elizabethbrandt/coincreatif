e36630f9f99c262b8bd6f7cfe896e734ef757da8
francine babauta <francine.babauta@outlook.com>
2021-03-22 20:48:13 -0700

    added checkout.html

--- client/src/index.js
+++ client/src/index.js
@@ -6,15 +6,36 @@ import reportWebVitals from './reportWebVitals';
 import { ThemeProvider } from "@material-ui/core";
 import theme from './theme';
 import * as serviceWorker from './serviceWorker';
+import stripePromise = loadStripe('process_env_STRIPE_PUBLIC_KEY);
+function App() {
+  const handleClick = async (event) => {
+    // Get Stripe.js instance
+    const stripe = await stripePromise;
 
-ReactDOM.render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    // Call your backend to create the Checkout Session
+    const response = await fetch('/create-checkout-session', { method: 'POST' });
+
+    const session = await response.json();
+
+    // When the customer clicks on the button, redirect them to Checkout.
+    const result = await stripe.redirectToCheckout({
+      sessionId: session.id,
+    });
+
+    if (result.error) {
+      // If `redirectToCheckout` fails due to a browser or network
+      // error, display the localized error message to your customer
+      // using `result.error.message`.
+    }
+  };
+
+  return (
+    <button role="link">
+      Checkout
+    </button>
+  );
+}
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
