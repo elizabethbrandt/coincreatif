072dcbc4870372ac64c29396402eaab3e40c7063
Jailanie Kaganovsky <jnctw33ty@gmail.com>
2021-03-24 23:10:18 -0700

    Removed extra padding from favorite button.

--- client/src/components/ProductCard/index.js
+++ client/src/components/ProductCard/index.js
@@ -1,6 +1,5 @@
 import { makeStyles } from '@material-ui/core/styles';
-import { Card, CardMedia, CardActions, CardContent, Button, Typography, IconButton } from '@material-ui/core';
-import InfoIcon from '@material-ui/icons/Info';
+import { Card, CardMedia, CardActions, CardContent, Typography, IconButton } from '@material-ui/core';
 import AddShoppingCartOutlinedIcon from '@material-ui/icons/AddShoppingCartOutlined';
 import FavoriteBtn from '../FavoriteBtn';
 import ProductModal from '../ProductModal';
@@ -34,9 +33,7 @@ const ProductCard = ({product}) => {
                 <IconButton>
                     <AddShoppingCartOutlinedIcon fontSize="large" />
                 </IconButton>
-                <IconButton>
-                    <FavoriteBtn />
-                </IconButton>
+                <FavoriteBtn />
             </CardActions>
         </Card>
     )
