07c927bf7ac6f5f0db256ea3caef2c3d8258fdae
Jailanie Kaganovsky <jnctw33ty@gmail.com>
2021-03-24 22:18:07 -0700

    Replaced View Item icon to ProductModal component.

--- client/src/components/ProductCard/index.js
+++ client/src/components/ProductCard/index.js
@@ -1,7 +1,9 @@
 import { makeStyles } from '@material-ui/core/styles';
 import { Card, CardMedia, CardActions, CardContent, Button, Typography, IconButton } from '@material-ui/core';
+import InfoIcon from '@material-ui/icons/Info';
 import AddShoppingCartOutlinedIcon from '@material-ui/icons/AddShoppingCartOutlined';
 import FavoriteBtn from '../FavoriteBtn';
+import ProductModal from '../ProductModal';
 
 const useStyles = makeStyles({
     media: {
@@ -26,12 +28,14 @@ const ProductCard = ({product}) => {
             </CardContent>
 
             <CardActions>
-                <Button size="small" variant="outlined" href="/">View Item</Button>
-                <IconButton aria-label="settings">
-                    <FavoriteBtn />
+                <IconButton>
+                    <ProductModal/>
                 </IconButton>
                 <IconButton>
                     <AddShoppingCartOutlinedIcon fontSize="large" />
+                </IconButton>
+                <IconButton>
+                    <FavoriteBtn />
                 </IconButton>
             </CardActions>
         </Card>
