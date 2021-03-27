665eb9323e11328fd0852547d0b5e9e21aa49da7
francine babauta <francine.babauta@outlook.com>
2021-03-27 06:32:13 -0700

    addtocart for productitems

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -1,5 +1,36 @@
-import React from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import CartItem from './CartItem';
 
-export default function Cart() {
-  return <>Cart</>
+export default class Cart extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { products: [], total: 0 }
+    }
+
+    componentDidMount() {
+        let cart = localStorage.getItem('cart');
+        if (!cart) return;
+        getCartProducts(cart).then((products) => {
+            let total = 0;
+            for (var i = 0; i < products.length; i++) {
+                total += products[i].price * products[i].qty;
+            }
+            this.setState({ products, total });
+            });
+    }
+
+    removeFromCart = (products) => {
+        let products = this.state.products.filter((item) => item.id !== product.id);
+        let cart = JSON.parse(localStorage.getItem('cart'));
+        delete cart[product.id.toString()];
+        localStorage.setItem('cart', JSON.stringify(cart));
+        let total = this.state.total - (product.qty * product.price)
+        this.setState({ products, total });
+    }
+
+    clearCart = () => {
+        localStorage.removeItem('cart');
+        this.setState({ products: [] });
+    }
 }
